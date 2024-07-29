5e66fb76a1b2352f49f3fd0ec8e1d3213b1d34d3
wildjcrt <wildjcrt@yahoo.com.tw>
2024-07-29 14:25:44 +0800

    Migrate some models
    * Term
    * Stem
    * Definition
    * Example
    * Description
    * Synonym

---
+++ app/models/description.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: descriptions
+#
+#  id            :integer          not null, primary key
+#  definition_id :integer
+#  term_id       :integer
+#  content       :string(500)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class Description < ApplicationRecord
+  belongs_to :term
+  belongs_to :definition
+  has_many   :examples
+  has_many   :synonyms
+end
---
+++ db/schema.rb
@@ -0,0 +1,71 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2024_07_29_060807) do
+  create_table "definitions", force: :cascade do |t|
+    t.integer "term_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["term_id"], name: "index_definitions_on_term_id"
+  end
+
+  create_table "descriptions", force: :cascade do |t|
+    t.integer "definition_id"
+    t.integer "term_id"
+    t.string "content", limit: 500
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["definition_id"], name: "index_descriptions_on_definition_id"
+    t.index ["term_id"], name: "index_descriptions_on_term_id"
+  end
+
+  create_table "examples", force: :cascade do |t|
+    t.integer "description_id"
+    t.string "content"
+    t.string "linked_content", limit: 500
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["description_id"], name: "index_examples_on_description_id"
+  end
+
+  create_table "stems", force: :cascade do |t|
+    t.string "name", limit: 40
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_stems_on_name"
+  end
+
+  create_table "synonyms", force: :cascade do |t|
+    t.integer "description_id"
+    t.string "term_type", limit: 5
+    t.string "content"
+    t.string "linked_content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["description_id"], name: "index_synonyms_on_description_id"
+  end
+
+  create_table "terms", force: :cascade do |t|
+    t.integer "stem_id"
+    t.string "name"
+    t.string "lower_name"
+    t.integer "repetition"
+    t.boolean "loanword", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["loanword"], name: "index_terms_on_loanword"
+    t.index ["lower_name"], name: "index_terms_on_lower_name"
+    t.index ["name"], name: "index_terms_on_name"
+    t.index ["stem_id"], name: "index_terms_on_stem_id"
+  end
+
+end
