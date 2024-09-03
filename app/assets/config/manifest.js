29837d55d4c25a0036a8f961705da885ab79c71c
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-03 15:02:28 +0800

    Add dartsass for application.scss and add assets/fonts

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
+// stylesheets 需同時設定在 config/initializers/dartsass.rb
+//= link_tree ../fonts
 //= link_tree ../images
-//= link_directory ../stylesheets .css
+//= link application.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
