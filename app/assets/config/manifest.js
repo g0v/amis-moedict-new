4a9073b270c63192d9998aa740010de17197eaac
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-04 13:32:26 +0800

    Remove unused `link application.css` in assets/config/manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 // stylesheets 需同時設定在 config/initializers/dartsass.rb
 //= link_tree ../fonts
 //= link_tree ../images
-//= link application.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
