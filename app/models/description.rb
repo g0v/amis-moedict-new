dbb24d675d7db2ade247e263cf0e2b697c7551e9
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-05 19:37:35 +0800

    Add #clean_content in Description, Example and Synonym models

--- app/models/description.rb
+++ app/models/description.rb
@@ -16,4 +16,12 @@ class Description < ApplicationRecord
   belongs_to :term
   has_many   :examples, dependent: :destroy
   has_many   :synonyms, dependent: :destroy
+
+  before_save :clean_content
+
+  private
+
+  def clean_content
+    self.content = content.gsub(/\xEF\xBF\xB9|\xEF\xBB\xBF|\xEF\xBF\xBA|\xEF\xBF\xBB/, '').strip if content.present?
+  end
 end
