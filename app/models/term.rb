0eef66512f047b65513c7cbadf48fd0fc5d443ee
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-05 11:34:27 +0800

    Run `annotate --models` to update Example, Synonym and Term models

--- app/models/term.rb
+++ app/models/term.rb
@@ -10,7 +10,6 @@
 #  name          :string
 #  lower_name    :string
 #  repetition    :integer
-#  loanword      :boolean          default(FALSE), not null
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #
