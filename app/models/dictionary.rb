7ba862b74dedc39fdd83ff45bd2d8bb4a9066cb1
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-06 12:40:20 +0800

    Complete dictionary#COLOR

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -16,7 +16,13 @@ class Dictionary < ApplicationRecord
   COLOR = {
     "蔡中涵大辭典" => "blue",
     "博利亞潘世光阿法字典" => "gray",
-    "方敏英字典" => "green"
+    "方敏英字典" => "green",
+    "學習詞表－秀姑巒阿美語" => "red",
+    "學習詞表－海岸阿美語" => "red",
+    "學習詞表－馬蘭阿美語" => "red",
+    "學習詞表－恆春阿美語" => "red",
+    "學習詞表－南勢阿美語" => "red",
+    "原住民族語言線上辭典" => "indigo"
   }.freeze
 
   def color
