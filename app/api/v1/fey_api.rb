57fd8ffb8b9ecbf80f9292c4fab8798c17c34a61
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-08 10:26:51 +0800

    Complete GET /api/v1/p/:name API

--- app/api/v1/fey_api.rb
+++ app/api/v1/fey_api.rb
@@ -2,34 +2,30 @@ module V1
   class FeyAPI < Base
     resources :p do
       params do
-        requires :name, type: String, desc: "詞彙，對應 Term#name"
+        requires :name, type: String, desc: "方敏英字典詞彙，對應 Term#name"
       end
       get ":name" do
-        {
-          h:    [
-            {
-              name: params[:name],
-              d:    [
-                {
-                  f:    "String, required",
-                  e:    [
-                    "String"
-                  ],
-                  s:    [
-                    "String"
-                  ],
-                  r:    [
-                    "String"
-                  ],
-                  type: "String, optional"
-                }
-              ]
-            }
-          ],
-          t:    "String, required",
-          stem: "String, optional",
-          tag:  "String, optional"
-        }
+        dictionary = Dictionary.find_by(name: "方敏英字典")
+        term = dictionary.terms.includes(:stem, descriptions: %i[examples synonyms]).find_by(name: params[:name])
+
+        if term.present?
+          result = { t: term.lower_name }
+          result[:stem] = term.stem.name if term.stem.present?
+
+          result[:h] = []
+          result[:h][0] = { d: [] }
+          result[:h][0][:name] = term.name if term.lower_name != term.name
+          term.descriptions.each do |description|
+            description_hash = { f: description.content }
+            description_hash[:e] = description.examples.map(&:content) if description.examples.present?
+            description_hash[:s] = description.synonyms.alts.map(&:content) if description.synonyms.alts.present?
+            result[:h][0][:d] << description_hash
+          end
+
+          result
+        else
+          { term: :not_found }
+        end
       end
     end
   end
