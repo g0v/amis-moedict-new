b3e9860380e68ed7464ce052f6e66a0c094492fd
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-11 02:47:20 +0800

    Move `config.web_console` from application to environments/development in config

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,5 @@ module AmisMoedict
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.web_console.permissions = "172.17.0.1"
   end
 end
