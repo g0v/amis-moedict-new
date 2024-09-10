85254c8d570937533afc5122e9a2f06e4458e9b8
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-10 12:51:06 +0800

    Add jquery and jquery-ui in config/importmap. Use autocomplete in javascript/application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "jquery-ui"
+
+$( "#search" ).autocomplete({
+  source: function( request, response ) {
+    $.getJSON( `/api/v2/searches/${request.term}`, {}, response );
+  },
+  select: function( event, ui ) {
+    this.value = ui.item.term;
+    return false;
+  }
+});
