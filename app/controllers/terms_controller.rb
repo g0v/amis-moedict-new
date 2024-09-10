a3f596649c8c65e8b4113b445c5705b352465eef
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-11 01:46:53 +0800

    Set @term for root page in terms#index

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class TermsController < ApplicationController
-  def index; end
+  def index
+    if session[:last_page]
+      redirect_to session[:last_page]
+    else
+      @terms = Term.includes(:dictionary, :stem, descriptions: %i[examples synonyms]).where(name: "co'ong").order(:dictionary_id)
+      redirect_to "/terms/co'ong"
+    end
+  end
 
   def show
     @terms = Term.includes(:dictionary, :stem, descriptions: %i[examples synonyms]).where(name: params[:id]).order(:dictionary_id)
     redirect_back(fallback_location: "/") if @terms.empty?
+
+    session[:last_page] = request.url
   end
 end
