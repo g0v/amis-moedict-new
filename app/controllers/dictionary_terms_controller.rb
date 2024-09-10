1f01718b65d52f32e6efa1e7bc2568e1243ead57
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-11 03:41:55 +0800

    Fix double render error in dictionary_terms#show

--- app/controllers/dictionary_terms_controller.rb
+++ app/controllers/dictionary_terms_controller.rb
@@ -1,10 +1,10 @@
 class DictionaryTermsController < ApplicationController
   def show
     @dictionary = Dictionary.find_by(id: params[:dictionary_id])
-    redirect_back(fallback_location: "/") if @dictionary.blank?
+    return redirect_back(fallback_location: "/") if @dictionary.blank?
 
     @terms = @dictionary.terms.includes(:dictionary, :stem, descriptions: %i[examples synonyms]).where(name: params[:id])
-    redirect_back(fallback_location: "/") if @terms.empty?
+    return redirect_back(fallback_location: "/") if @terms.empty?
 
     session[:last_page] = request.url
 
