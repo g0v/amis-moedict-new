50a348b23a4d1e386fec1fc990fd9faec2466f7c
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-11 03:33:54 +0800

    Run `bin/precompile`

---
+++ public/assets/application-03866108a6f71bf1dda4729a4b936412c04f5c211579b2c4467a33de2269bd21.js
@@ -0,0 +1,30 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "jquery"
+import "jquery-ui"
+
+$( "#search" ).autocomplete({
+  source: function( request, response ) {
+    $.getJSON( `/api/v2/searches/${request.term}`, {}, response );
+  },
+  focus: function( event, ui ) {
+    $( "#search" ).val();
+    return false;
+  },
+  select: function( event, ui ) {
+    this.value = ui.item.term;
+    window.location.href = `/terms/${ui.item.term}`;
+    return false;
+  }
+}).autocomplete( "instance" )._renderItem = function( ul, item ) {
+  return $( `<li class="ui-menu-item">` )
+    .append( `<div class="flex justify-between ui-menu-item-wrapper px-2 py-0.5 text-gray-600 hover:bg-gray-200">
+                <div class"text-sm font-medium text-indigo-600">${item.term}</div>
+                <div class="text-sm text-gray-500 mt-1 sm:mt-0">${item.description}</div>
+              </div>` )
+    .appendTo( ul );
+};
+
+$( "#sidebar" ).append( $( "#ui-id-1.ui-autocomplete" ) );
+$( "#ui-id-1.ui-autocomplete" ).addClass( "bg-white" );
