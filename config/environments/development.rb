c31229e84e057bf7ec0ff69fc8e7aa05b37aa7a8
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-10 20:18:55 +0800

    Change config.assets.prefix in config/environments/development
    This project will run `assets:precompile` before deploying by `git push` to middle2.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -66,6 +66,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # The public path that Sprockets uses by default is /assets.
+  config.assets.prefix = "/dev-assets"
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
