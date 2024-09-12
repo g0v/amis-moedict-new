50cf7c928a1d7c0a5e63ffadec7f07b6da44bf62
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-12 20:58:53 +0800

    Fix TermsHelper#linked_term

--- app/helpers/terms_helper.rb
+++ app/helpers/terms_helper.rb
@@ -2,19 +2,54 @@ module TermsHelper
   def linked_term(record:, dictionary:)
     html = ""
 
-    parts = record.content.split(/[`~]/).reject(&:empty?)
-    parts.each_with_index do |part, i|
-      if part.match(/^[\p{Alnum}]+$/) && part != params[:id]
-        term_path = if i == 0
-                      "terms/#{part.downcase}"
-                    else
-                      "terms/#{part}"
-                    end
-        term_path = "dictionaries/#{dictionary.id}/#{term_path}" if dictionary.present?
+    record.content.split(/(\s)/).each_with_index do |term, i|
+      linked_terms = term.scan(/`([^`~]+)~/).flatten
+      pattern = Regexp.union(linked_terms)
 
-        html += "<a href=\"/#{term_path}\" class=\"hoverable-term border-b border-gray-300 text-gray-800 hover:bg-gray-300 focus:bg-gray-300 hover:text-[#0070a3] focus:text-[#0070a3] focus:outline focus:outline-1 focus:outline-[#69d2fc]\">#{part}</a>"
+      pure_term = term.gsub(/\`|\~/, "")
+      term_parts = pure_term.split(/(#{pattern})/).reject(&:empty?)
+      if i == 0 # 句子第一個字的 term 需要連結時，要把 term_part 轉成小寫
+        term_parts.each_with_index do |term_part, j|
+          if linked_terms.include?(term_part) &&
+             term_part.match(/^[\p{Alnum}'^]+$/) && # 用 regex 跳過中文
+             term_part != params[:id] # 本頁的詞不需要連結
+
+            # 產生網址用的 term_part_for_url
+            # 遇到 term_part 是句首時，將 term_part 轉成小寫
+            term_part_for_url = if pure_term[0..term_part.size-1] == term_part
+                                  term_part.downcase
+                                else
+                                  term_part
+                                end
+
+            if dictionary.present? # 網址有指定字典時，路徑也要在網址內
+              term_path = dictionary_term_path(dictionary.id, term_part_for_url)
+            else
+              term_path = term_path(term_part_for_url)
+            end
+
+            html += "<a href=\"#{term_path}\" data-term=\"#{term_part}\" class=\"hoverable-term border-b border-gray-300 text-gray-800 hover:bg-gray-300 focus:bg-gray-300 hover:text-[#0070a3] focus:text-[#0070a3] focus:outline focus:outline-1 focus:outline-[#69d2fc]\">#{term_part}</a>"
+          else
+            html += term_part
+          end
+        end
       else
-        html += part
+        term_parts.each do |term_part|
+          if linked_terms.include?(term_part) &&
+             term_part.match(/^[\p{Alnum}'^]+$/) && # 用 regex 跳過中文
+             term_part != params[:id] # 本頁的詞不需要連結
+
+            if dictionary.present? # 網址有指定字典時，路徑也要在網址內
+              term_path = dictionary_term_path(dictionary.id, term_part)
+            else
+              term_path = term_path(term_part)
+            end
+
+            html += "<a href=\"#{term_path}\" data-term=\"#{term_part}\" class=\"hoverable-term border-b border-gray-300 text-gray-800 hover:bg-gray-300 focus:bg-gray-300 hover:text-[#0070a3] focus:text-[#0070a3] focus:outline focus:outline-1 focus:outline-[#69d2fc]\">#{term_part}</a>"
+          else
+            html += term_part
+          end
+        end
       end
     end
 
