45a4e523fed81d1ad1c77e6018807e6a34cd623b
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-14 22:46:43 +0800

    Run `bin/precompile`

--- public/assets/application-2b4041aec89b641772a94585eac2d295649a4335b92b322800b080fe8068eda1.js
+++ public/assets/application-2b4041aec89b641772a94585eac2d295649a4335b92b322800b080fe8068eda1.js
@@ -5,6 +5,8 @@ import "jquery"
 import "jquery-ui"
 import "sv-hover-intent"
 
+// 搜尋功能 START
+
 $( "#search" ).autocomplete({
   source: function( request, response ) {
     $.getJSON( `/api/v2/searches/${request.term}`, {}, response );
@@ -14,16 +16,19 @@ $( "#search" ).autocomplete({
     return false;
   },
   select: function( event, ui ) {
-    if ((ui.item.term !== "Awaay to. 找不到") && (ui.item.term.indexOf("精確搜尋，結果較少") === -1)) {
+    if ((ui.item.term !== "Awaay. 找不到。") && (ui.item.term.indexOf("精確搜尋，結果較少") === -1)) {
       this.value = ui.item.term;
       window.location.href = `/terms/${ui.item.term}`;
     }
     return false;
+  },
+  classes: {
+    "ui-autocomplete": "border border-gray-200 bg-gray-200 sm:border-0 sm:bg-inherit"
   }
 }).autocomplete( "instance" )._renderItem = function( ul, item ) {
   var divHtml;
 
-  if ((item.term !== "Awaay to. 找不到") && (item.term.indexOf("精確搜尋，結果較少") === -1)) {
+  if ((item.term !== "Awaay. 找不到。") && (item.term.indexOf("精確搜尋，結果較少") === -1)) {
     divHtml = `<div class="flex justify-between ui-menu-item-wrapper px-2 py-0.5 text-gray-600 hover:bg-gray-200">
                 <div class"text-sm font-medium text-indigo-600">${item.term}</div>
                 <div class="text-sm text-gray-500 mt-1 sm:mt-0">${item.description}</div>
@@ -38,6 +43,10 @@ $( "#search" ).autocomplete({
 };
 
 $( "#sidebar" ).append( $( "#ui-id-1.ui-autocomplete" ) );
+
+// 搜尋功能 END
+
+// 游標 hover 顯示詞義 START
 
 $( ".hoverable-term" ).tooltip({
   disabled: true, // 交給 hoverintent 處理 show/hide
@@ -86,3 +95,24 @@ new SV.HoverIntent( $( ".hoverable-term" ), {
   interval: 250,
   sensitivity: 7,
 });
+
+// 游標 hover 顯示詞義 END
+
+// PWA 手機版複製網址功能 START
+$( "#copy-url" ).tooltip({
+  disabled: true,
+  items: "button",
+  classes: {
+    "ui-tooltip": "shadow-xl rounded bg-gray-200 text-sm w-fit p-1"
+  },
+  content: "網址已複製"
+});
+
+$( "#copy-url" ).on( "click", function(){
+  navigator.clipboard.writeText(`${window.location.origin}${window.location.pathname}`);
+  $(this).tooltip( "option", { disabled: false }).tooltip( "open" );
+  setTimeout( function(){
+    $( "#copy-url" ).tooltip( "option", { disabled: true }).tooltip( "close" );
+  }, "500");
+});
+// PWA 手機版複製網址功能 START;
