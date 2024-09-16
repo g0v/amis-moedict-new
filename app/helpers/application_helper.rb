52f53e7dfa79bd22ce7ddb955d4ad1f18d7e3072
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-16 21:47:13 +0800

    Complete meta in application layout header and application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,35 +2,37 @@
 
 module ApplicationHelper
   def title(page_title = nil)
-    site_name = "阿美語萌典"
-
     if page_title.present?
       content_for(:title, page_title)
     else
       if content_for?(:title)
-        content_for(:title) + " - #{site_name}"
+        content_for(:title) + " - 阿美語萌典"
       else
-        site_name
+        "阿美語萌典"
       end
     end
   end
 
-  def description(desc = nil, type: :meta)
+  def description(desc = nil)
     if desc.present?
       content_for(:meta_description, desc)
     else
       if content_for?(:meta_description)
-        if type != :og
-          content_for(:meta_description)
-        else
-          if content_for?(:og_description)
-            content_for(:og_description)
-          else
-            safe_truncate_content(content_for(:meta_description), 200)
-          end
-        end
+        content_for(:meta_description)
       else
         "阿美語萌典是一本線上的族語辭典，提供多項方便功能，有助於學習族語者和教學者更輕鬆地找到需要的詞彙和例句，以及原住民族語言保存與發揚。"
+      end
+    end
+  end
+
+  def canonical(url = nil)
+    if url.present?
+      content_for(:canonical, url)
+    else
+      if content_for?(:canonical)
+        content_for(:canonical)
+      else
+        request.url
       end
     end
   end
