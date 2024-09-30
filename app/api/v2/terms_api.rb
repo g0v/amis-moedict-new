0f8753eb49e3ed0f05971a1f36216a0afc3b940e
wildjcrt <wildjcrt@yahoo.com.tw>
2024-09-30 10:34:33 +0800

    Use #imageN_url and #imageN_text in terms/show and api/v2/terms_api

--- app/api/v2/terms_api.rb
+++ app/api/v2/terms_api.rb
@@ -33,7 +33,9 @@ module V2
               description_hash[:type] = description.description_type if description.description_type.present?
               description_hash[:glossary_serial] = description.glossary_serial if description.glossary_serial.present?
               description_hash[:glossary_level] = description.glossary_level if description.glossary_level.present?
-              description_hash[:image] = description.image if description.image.present?
+              description_hash[:image1] = description.image1_url
+              description_hash[:image2] = description.image2_url
+              description_hash[:image3] = description.image3_url
 
               description.examples.each do |example|
                 example_hash = { content: example.content }
