75775e25e5c644d48f98d21aaf56c7a57fb605eb
wildjcrt <wildjcrt@yahoo.com.tw>
2024-10-02 11:37:57 +0800

    Run `bin/precompile`

---
+++ public/assets/application-ea8fe12814e5fa093c856439dea8e816a5b68362378bf10e0c013cd8e79315f9.js
@@ -0,0 +1,98 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "jquery"
+import "jquery-ui"
+
+document.addEventListener( "turbo:load", function() {
+  // 搜尋功能 START
+  $( "#search" ).autocomplete({
+    source: function( request, response ) {
+      $.getJSON( `/api/v2/searches/${request.term}`, {}, response );
+    },
+    focus: function( event, ui ) {
+      $( "#search" ).val();
+      return false;
+    },
+    select: function( event, ui ) {
+      if ((ui.item.term !== "Awaay. 找不到。") && (ui.item.term.indexOf("精確搜尋，結果較少") === -1)) {
+        this.value = ui.item.term;
+        window.location.href = `/terms/${ui.item.term}`;
+      }
+      return false;
+    },
+    classes: {
+      "ui-autocomplete": "border border-gray-200 bg-gray-200 sm:border-0 mr-8 sm:bg-inherit sm:w-1/4 sm:pr-8"
+    }
+  }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+    var divHtml;
+
+    if ((item.term !== "Awaay. 找不到。") && (item.term.indexOf("精確搜尋，結果較少") === -1)) {
+      divHtml = `<div class="flex justify-between ui-menu-item-wrapper px-2 py-0.5 text-gray-600 hover:bg-gray-200">
+                  <div class"text-sm font-medium text-indigo-600">${item.term}</div>
+                  <div class="text-sm text-gray-500 mt-1 sm:mt-0">${item.description}</div>
+                 </div>`;
+    } else {
+      divHtml = item.term;
+    }
+
+    return $( `<li class="ui-menu-item">` )
+             .append( divHtml )
+             .appendTo( ul );
+  };
+  // 搜尋功能 END
+
+  // 游標 hover 顯示詞義 START
+  $( ".hoverable-term" ).tooltip({
+    show: { delay: 400, duration: 0 },
+    items: "a",
+    classes: {
+      "ui-tooltip": "shadow-xl rounded-lg bg-gray-100 max-w-xs w-fit p-2"
+    },
+    open: function( event, ui ) {
+      var term, $this = $(this);
+      term = $(this).data('term');
+      $.getJSON( `/api/v2/terms/${term}`, {}, function(data) {
+        if (data.length !== undefined) {
+          var content = `<h3 class="text-xl">${term}</h3>
+                         <hr class="h-px border-0 dark:bg-gray-400">
+                         <ol class="list-decimal list-outside space-y-2 mt-2 ml-5">`;
+
+          data.slice(0, 3).forEach(function(termData) {
+            termData.descriptions.forEach(function(descriptionData) {
+              content += `<li class="text-sm">${descriptionData.content.replace(/[`~]/g, "")}</li>`;
+            });
+          });
+          content += "</ol>";
+
+          $this.tooltip("option", {
+            content: content,
+          });
+        } else {
+          $this.tooltip( "close" );
+        }
+      } );
+    },
+    content: "讀取中……"
+  });
+  // 游標 hover 顯示詞義 END
+
+  // PWA 手機版複製網址功能 START
+  $( "#copy-url" ).tooltip({
+    disabled: true,
+    items: "button",
+    classes: {
+      "ui-tooltip": "shadow-xl rounded bg-gray-200 text-sm w-fit p-1"
+    },
+    content: "網址已複製"
+  });
+
+  $( "#copy-url" ).on( "click", function(){
+    navigator.clipboard.writeText(`${window.location.origin}${window.location.pathname}`);
+    $(this).tooltip( "option", { disabled: false }).tooltip( "open" );
+    setTimeout( function(){
+      $( "#copy-url" ).tooltip( "option", { disabled: true }).tooltip( "close" );
+    }, "500");
+  });
+  // PWA 手機版複製網址功能 START
+});
---
+++ public/assets/manifest-a1a3e79e9738b44bc5256e2cf8811cfc8b494e8a2b1ae28bf90bc8a97a52d409.js
@@ -0,0 +1,6 @@
+// stylesheets 需同時設定在 config/initializers/dartsass.rb
+
+
+
+
+;
