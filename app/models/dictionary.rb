6ae1ab028842ece01e4eaf373da8717898948c19
wildjcrt <wildjcrt@yahoo.com.tw>
2025-04-29 20:28:14 +0800

    Add 博利亞潘世光阿漢字典 in view

--- app/models/dictionary.rb
+++ app/models/dictionary.rb
@@ -14,15 +14,16 @@ class Dictionary < ApplicationRecord
   has_many :terms
 
   COLOR = {
-    "蔡中涵大辭典"      => "blue",
-    "博利亞潘世光阿法字典"  => "gray",
-    "方敏英字典"       => "green",
+    "蔡中涵大辭典" => "blue",
+    "博利亞潘世光阿法字典" => "gray",
+    "方敏英字典" => "green",
     "學習詞表－秀姑巒阿美語" => "red",
-    "學習詞表－海岸阿美語"  => "red",
-    "學習詞表－馬蘭阿美語"  => "red",
-    "學習詞表－恆春阿美語"  => "red",
-    "學習詞表－南勢阿美語"  => "red",
-    "原住民族語言線上辭典"  => "indigo"
+    "學習詞表－海岸阿美語" => "red",
+    "學習詞表－馬蘭阿美語" => "red",
+    "學習詞表－恆春阿美語" => "red",
+    "學習詞表－南勢阿美語" => "red",
+    "原住民族語言線上辭典" => "indigo",
+    "博利亞潘世光阿漢字典" => "gray",
   }.freeze
 
   def self.ransackable_attributes(auth_object = nil)
