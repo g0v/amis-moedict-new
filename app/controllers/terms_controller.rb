35165f8b587ba99b76cee53079ea2eddbec79ee8
wildjcrt <wildjcrt@yahoo.com.tw>
2025-11-01 20:29:12 +0800

    Change to merge terms with same dictionary in terms#show

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -14,6 +14,14 @@ class TermsController < ApplicationController
     @terms = Term.includes(:dictionary, :stem, descriptions: %i[examples synonyms]).where(name: params[:id]).order(:dictionary_id)
     return redirect_back(fallback_location: "/") if @terms.empty?
 
+    arel_dictionary = Dictionary.arel_table
+    id_case_statement = Arel::Nodes::Case.new(arel_dictionary[:id])
+    [1, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10].each_with_index do |id, i|
+      id_case_statement.when(id).then(i+1)
+    end
+    id_case_statement.else(10000)
+
+    @dictionaries = Dictionary.where(id: @terms.map(&:dictionary_id).uniq).order(id_case_statement)
     session[:last_page] = request.url
   end
 end
