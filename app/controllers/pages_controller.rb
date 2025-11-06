6ac1b2bb74e367904a96225f6121f73852bfd766
wildjcrt <wildjcrt@yahoo.com.tw>
2025-11-06 15:05:50 +0800

    Create pages#statistics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,20 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
+  def statistics
+    @dictionaries = Dictionary.includes(:terms).all.sort_by { |d| -d.terms.size }
+
+    @examples_by_dict = Example.joins(description: { term: :dictionary })
+                               .group('dictionaries.id')
+                               .count
+
+    @synonyms_by_dict = Synonym.joins(description: { term: :dictionary })
+                               .group('dictionaries.id')
+                               .count
+
+    @total_terms = Term.count
+    @total_examples = Example.count
+    @total_synonyms = Synonym.count
+    @total_descriptions = Description.count
+  end
 end
