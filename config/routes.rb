c51713bcc5cb75c32a770c764b3030308c0ec5af
wildjcrt <wildjcrt@yahoo.com.tw>
2025-11-06 19:32:15 +0800

    Add /info page to navbar in application.html

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get "bookmarks"  => "pages#bookmarks",  as: :bookmarks
   get "about"      => "pages#about",      as: :about
   get "statistics" => "pages#statistics", as: :statistics
+  get "info"       => "pages#info",       as: :info
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
